b298fd4aaabae9a702a3a78ca99d8f3b7d79c9a5
CodeQ09 <kagisojiyane28@gmail.com>
2025-09-13 11:28:31 +0200

    chore: update testing configuration and dependencies
    - Modified the test script in package.json to separate CI coverage tests.
    - Added new testing libraries: @testing-library/jest-native, @testing-library/react-native, and react-test-renderer.
    - Created a new jest.config.ts file to manage Jest configuration, including coverage settings and transform ignore patterns.
    - Added jest.setup.ts for extending Jest matchers with testing-library.

---
+++ jest.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "jest"
+
+const config: Config = {
+  preset: "jest-expo",
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{ts,tsx,js,jsx}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/expo-env.d.ts',
+    '!**/.expo/**',
+  ],
+  coverageDirectory: 'coverage',
+  transformIgnorePatterns:  [
+      "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)"
+    ],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/coverage/'],
+}
+
+export default config
