617a2a9bef2a2904ae471aa6a79621ee469e51d2
CodeQ09 <kagisojiyane28@gmail.com>
2025-10-01 18:33:56 +0200

    fix: removed plugins

--- apps/mobile/eslint.config.js
+++ apps/mobile/eslint.config.js
@@ -2,14 +2,14 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
-const tsPlugin = require("@typescript-eslint/eslint-plugin")
-const importPlugin = require("eslint-plugin-import")
+// const tsPlugin = require("@typescript-eslint/eslint-plugin")
+// const importPlugin = require("eslint-plugin-import")
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
-    plugins: {'@typescript-eslint': tsPlugin, 'import': importPlugin},
+    // plugins: {'@typescript-eslint': tsPlugin, 'import': importPlugin},
     settings: {
       'import/resolver': {
         typescript: {
