06093131b6f72bbaad41f103a08ef4309f25f739
CodeQ09 <kagisojiyane28@gmail.com>
2025-10-01 18:28:14 +0200

    fix: used flat config plugins for eslint

--- apps/mobile/eslint.config.js
+++ apps/mobile/eslint.config.js
@@ -2,11 +2,14 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
+const tsPlugin = require("@typescript-eslint/eslint-plugin")
+const importPlugin = require("eslint-plugin-import")
+
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
-    plugins: ['@typescript-eslint', 'import'],
+    plugins: {'@typescript-eslint': tsPlugin, 'import': importPlugin},
     settings: {
       'import/resolver': {
         typescript: {
